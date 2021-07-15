064ad47434a97d449c512dac35f94e97db0bb6ba
Devansh Raghav - @vip3r <79361298+DevanshRaghav75@users.noreply.github.com>
2021-07-15 19:47:20 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "grepX",
+    version = "1.0",
+    author = "Devansh Raghav",
+    author_email = "indiananonymous75@gmail.com",
+    description = ("grep pattterns"),
+    license = "MIT",
+    keywords = ["grepX", "Bug Bounty", "pentesting", "security", "hacking"],
+    url = "https://github.com/DevanshRaghav75/grepX",
+    packages=find_packages(),
+    long_description=read('README.md'),
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Topic :: Security",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+    ],
+    install_requires=[
+        'colorama',
+    ],
+
+    entry_points={
+        'console_scripts': [
+            'grepx = grepX.__main__:grep'
+        ]
+    },
+)
