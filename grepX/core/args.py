9b626f47050e7c0fb44a31a19df8a6026c6f5d2a
Devansh Raghav - @vip3r <79361298+DevanshRaghav75@users.noreply.github.com>
2021-07-16 00:28:25 +0700

    Delete grepX directory

--- grepX/core/args.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="grep patterns")
-parser.add_argument('file', help="Specify the file")
-parser.add_argument('pattern', help="Specify the pattern")
-args = parser.parse_args()
-FILE = args.file
-PATTERN = args.pattern
